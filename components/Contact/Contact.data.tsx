20b15329aa61ee9c11fc70efe9c8a185b3cbef82
JMRamosJuarez <jesusmanuelramosjuarez@gmail.com>
2026-01-14 19:25:02 -0700

    Fix ContactData optional values

--- components/Contact/Contact.data.tsx
+++ components/Contact/Contact.data.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Text, View } from 'react-native';
 
+import { eDepartment } from '@contacts/Contacts.types';
 import { useContactById } from '@contacts/selectors/Contact.selectors';
 
 const ContactData: React.FC<{
@@ -12,10 +13,10 @@ const ContactData: React.FC<{
   const { t } = useTranslation('departments');
   return (
     <View style={{ flex: 1 }}>
-      <Text>{contact.name}</Text>
-      <Text>{contact.email}</Text>
-      {contact.phone && <Text>{contact.phone}</Text>}
-      <Text>{t(contact.department)}</Text>
+      <Text>{contact?.name}</Text>
+      <Text>{contact?.email}</Text>
+      {contact?.phone && <Text>{contact.phone}</Text>}
+      <Text>{t(contact?.department || eDepartment.UNKNOWN)}</Text>
     </View>
   );
 };
